e5eda3bad17e547c106d00632ac413f1a3b8d412
shatakshisinha16 <shatakshisinha7642@gmail.com>
2025-07-24 20:33:44 +0530

    error handling

--- backend/jobportal/urls.py
+++ backend/jobportal/urls.py
@@ -3,8 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core.views import JobListView
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+def custom_bad_request(request, exception):
+    return JsonResponse({'error': 'Bad Request (malformed JSON or missing fields)'}, status=400)
+
+handler400 = custom_bad_request
